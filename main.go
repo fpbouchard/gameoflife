2892c3fcd80589f845c2e0d9b05676b3e18c9786
François-Pierre Bouchard <fp@valian.ca>
2023-08-27 18:46:57 -0400

    feat: add fun stuff

--- main.go
+++ main.go
@@ -2,13 +2,19 @@
 package main
 
 import (
+	"fmt"
 	"image/color"
 	"log"
 	"time"
 
 	"github.com/hajimehoshi/ebiten/v2"
+	"github.com/hajimehoshi/ebiten/v2/ebitenutil"
+	"github.com/hajimehoshi/ebiten/v2/examples/resources/fonts"
 	"github.com/hajimehoshi/ebiten/v2/inpututil"
+	"github.com/hajimehoshi/ebiten/v2/text"
 	"github.com/hajimehoshi/ebiten/v2/vector"
+	"golang.org/x/image/font"
+	"golang.org/x/image/font/opentype"
 )
 
 const (
@@ -21,6 +27,10 @@ const (
 	patternEditorScale  = 20
 )
 
+var (
+	mplusNormalFont font.Face
+)
+
 type Game struct {
 	// the cells, a 2d array of logicalScreenWidth x logicalScreenHeight of bools
 	cells          [][]bool
@@ -28,36 +38,81 @@ type Game struct {
 	patternWidth   int
 	patternHeight  int
 	lastUpdateTime time.Time
+	welcomeScreen  bool
 	active         bool
 	editorVisible  bool
+	speed          time.Duration
+	terminated     bool
 }
 
-func (g *Game) Update() error {
-	timeDelta := time.Since(g.lastUpdateTime)
-
+func (g *Game) ManageKeys() {
 	if inpututil.IsKeyJustPressed(ebiten.KeyEscape) {
-		g.initCells()
-	}
-
-	if inpututil.IsKeyJustPressed(ebiten.KeyBackspace) {
-		g.initPattern()
+		g.terminated = true
 	}
 
 	if inpututil.IsKeyJustPressed(ebiten.KeySpace) {
-		g.active = !g.active
+		if g.welcomeScreen {
+			g.welcomeScreen = false
+		} else {
+			g.active = !g.active
+		}
+	}
+
+	if inpututil.IsKeyJustPressed(ebiten.KeyBackspace) {
+		g.initCells()
+	}
+
+	if inpututil.IsKeyJustPressed(ebiten.KeyDelete) {
+		g.initPattern(false)
 	}
 
 	if inpututil.IsKeyJustPressed(ebiten.KeyTab) {
 		g.editorVisible = !g.editorVisible
 	}
 
+	if inpututil.IsKeyJustPressed(ebiten.KeyEqual) {
+		g.speed -= 25
+		if g.speed < 0 {
+			g.speed = 0
+		}
+	} else if inpututil.IsKeyJustPressed(ebiten.KeyMinus) {
+		g.speed += 25
+	}
+
+	if inpututil.IsKeyJustPressed(ebiten.KeyUp) {
+		g.patternHeight--
+		if g.patternHeight < 1 {
+			g.patternHeight = 1
+		}
+		g.initPattern(true)
+	} else if inpututil.IsKeyJustPressed(ebiten.KeyDown) {
+		g.patternHeight++
+		g.initPattern(true)
+	} else if inpututil.IsKeyJustPressed(ebiten.KeyLeft) {
+		g.patternWidth++
+		g.initPattern(true)
+	} else if inpututil.IsKeyJustPressed(ebiten.KeyRight) {
+		g.patternWidth--
+		if g.patternWidth < 1 {
+			g.patternWidth = 1
+		}
+		g.initPattern(true)
+	}
+}
+
+func (g *Game) Update() error {
+	timeDelta := time.Since(g.lastUpdateTime)
+
+	g.ManageKeys()
+	if g.terminated {
+		return ebiten.Termination
+	}
+
 	if g.editorVisible && inpututil.IsMouseButtonJustPressed(ebiten.MouseButtonLeft) {
 		x, y := ebiten.CursorPosition()
 		patternEditorX := logicalScreenWidth - (g.patternWidth * patternEditorScale)
 		patternEditorY := g.patternHeight * patternEditorScale
-		// If the cursor is within the pattern editor, toggle the pattern pixel
 		if x >= patternEditorX && y <= patternEditorY {
-			// Translate the cursor position to the pattern editor (0,0) is the top left corner, (patternSize - 1, patternSize - 1) is the bottom right corner
 			x = (x - patternEditorX) / patternEditorScale
 			y = y / patternEditorScale
 			g.pattern[y][x] = !g.pattern[y][x]
@@ -71,19 +126,15 @@ func (g *Game) Update() error {
 		if !g.editorVisible || x < patternEditorX || y > patternEditorY {
 			x = x / logicalScreenFactor
 			y = y / logicalScreenFactor
-			// Print the pattern at the cursor position (aligned top-left)
 			for i := 0; i < g.patternHeight; i++ {
 				for j := 0; j < g.patternWidth; j++ {
-					if g.pattern[i][j] {
-						g.cells[y+i-g.patternHeight][x+j-g.patternWidth] = true
-					}
+					g.cells[y+i-g.patternHeight][x+j-g.patternWidth] = g.pattern[i][j]
 				}
 			}
 		}
 	}
 
-	// Apply rules of life once every 10 frames
-	if g.active && timeDelta >= 25*time.Millisecond {
+	if g.active && timeDelta >= g.speed*time.Millisecond {
 		g.lastUpdateTime = time.Now()
 
 		// Rules of life:
@@ -134,9 +185,25 @@ func (g *Game) Update() error {
 }
 
 func (g *Game) Draw(screen *ebiten.Image) {
-	// Draw a blue square at the cursor position
-	x, y := ebiten.CursorPosition()
-	vector.DrawFilledRect(screen, float32(x-cellSize/2), float32(y-cellSize/2), cellSize, cellSize, color.RGBA{0, 0, 255, 255}, false)
+	if g.welcomeScreen {
+		// Draw a black background
+		vector.DrawFilledRect(screen, 0, 0, logicalScreenWidth, logicalScreenHeight, color.Black, false)
+		// Draw the instructions at the center of the screen
+		ebitenutil.DebugPrintAt(screen, "Welcome to the Game of Life!\n\n"+
+			"Press <space> to start (and pause)\n"+
+			"Press <backspace> to clear the screen\n"+
+			"Press <delete> to clear the pattern\n"+
+			"Press <tab> to show/hide the pattern editor\n"+
+			"Press <up>/<down>/<left>/<right> to change the pattern size\n"+
+			"Press <+>/<-> to change the speed\n"+
+			"Click (and drag) to add the pattern to the screen\n"+
+			"Press <escape> to quit\n",
+			20, 20,
+		)
+
+		return
+	}
+
 	// Draw a pixel at each cell that is alive
 	for i := 0; i < logicalScreenHeight; i++ {
 		for j := 0; j < logicalScreenWidth; j++ {
@@ -148,17 +215,25 @@ func (g *Game) Draw(screen *ebiten.Image) {
 		}
 	}
 
+	// Draw pattern editor
 	if g.editorVisible {
-		vector.DrawFilledRect(screen, logicalScreenWidth-float32(g.patternWidth)*patternEditorScale, 0, logicalScreenWidth, float32(g.patternHeight)*patternEditorScale, color.Black, false)
+		patternEditorWidth := g.patternWidth * patternEditorScale
+		patternEditorHeight := g.patternHeight * patternEditorScale
+		vector.DrawFilledRect(screen, logicalScreenWidth-float32(g.patternWidth)*patternEditorScale, 0, float32(patternEditorWidth), float32(patternEditorHeight), color.Black, false)
 		for i := 0; i < g.patternHeight; i++ {
 			for j := 0; j < g.patternWidth; j++ {
 				x := float32(logicalScreenWidth - ((g.patternWidth - j) * patternEditorScale))
 				y := float32(i * patternEditorScale)
-				vector.StrokeRect(screen, x, y, patternEditorScale, patternEditorScale, 1, color.White, false)
+				vector.StrokeRect(screen, x, y, patternEditorScale, patternEditorScale, 1, color.RGBA{127, 127, 127, 255}, false)
 				if g.pattern[i][j] {
 					vector.DrawFilledRect(screen, x, y, patternEditorScale, patternEditorScale, color.White, false)
 				}
 			}
+			// Display the pattern size
+			msg := fmt.Sprintf("%dx%d", g.patternWidth, g.patternHeight)
+			textBounds := text.BoundString(mplusNormalFont, msg)
+			// Center the text under the pattern editor
+			text.Draw(screen, msg, mplusNormalFont, (logicalScreenWidth-patternEditorWidth/2)-(textBounds.Dx()/2), patternEditorHeight+10, color.White)
 		}
 
 	}
@@ -175,25 +250,33 @@ func (g *Game) Layout(outsideWidth, outsideHeight int) (screenWidth, screenHeigh
 }
 
 func (g *Game) initCells() {
-	// Reinitialize cells array
 	g.cells = make([][]bool, logicalScreenHeight)
 	for i := range g.cells {
 		g.cells[i] = make([]bool, logicalScreenWidth)
 	}
 }
 
-func (g *Game) initPattern() {
-	// Reinitialize cells array
-	g.pattern = make([][]bool, g.patternHeight)
-	for i := range g.pattern {
-		g.pattern[i] = make([]bool, g.patternWidth)
+func (g *Game) initPattern(keepPrevious bool) {
+	newPattern := make([][]bool, g.patternHeight)
+	for i := range newPattern {
+		newPattern[i] = make([]bool, g.patternWidth)
 	}
+	if keepPrevious {
+		for i := 0; i < g.patternHeight; i++ {
+			for j := 0; j < g.patternWidth; j++ {
+				if i < len(g.pattern) && j < len(g.pattern[i]) {
+					newPattern[i][j] = g.pattern[i][j]
+				}
+			}
+		}
+	}
+	g.pattern = newPattern
 }
 
 func (g *Game) initGlider() {
 	g.patternWidth = 3
 	g.patternHeight = 3
-	g.initPattern()
+	g.initPattern(false)
 	// Build a basic glider pattern
 	// 0 1 0
 	// 0 0 1
@@ -205,20 +288,38 @@ func (g *Game) initGlider() {
 	g.pattern[2][2] = true
 }
 
+func loadFont() {
+	tt, err := opentype.Parse(fonts.MPlus1pRegular_ttf)
+	if err != nil {
+		log.Fatal(err)
+	}
+	const dpi = 72
+	mplusNormalFont, err = opentype.NewFace(tt, &opentype.FaceOptions{
+		Size:    9,
+		DPI:     dpi,
+		Hinting: font.HintingVertical,
+	})
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
 func main() {
-	// Initialize the Game struct
+	loadFont()
 	g := &Game{
 		lastUpdateTime: time.Now(),
-		active:         true,
+		welcomeScreen:  true,
+		active:         false,
 		editorVisible:  true,
 		patternWidth:   3,
 		patternHeight:  3,
+		speed:          25,
 	}
 	g.initCells()
 	g.initGlider()
 
 	ebiten.SetWindowSize(screenWidth, screenHeight)
-	ebiten.SetWindowTitle("Hello, World!")
+	ebiten.SetWindowTitle("Game of Life")
 	if err := ebiten.RunGame(g); err != nil {
 		log.Fatal(err)
 	}
